2fef2e68d4e49931e9127c72e7d6aefcb24433f4
Lan Nguyen <nguyenlan13@gmail.com>
2019-06-27 13:43:46 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, female, pink, lazy, yes);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, male, blue, old, yes);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, female, red, happy, yes);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, female, green, crazy, yes);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, male, green, rowdy, yes);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, female, purple, nice, yes);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, male, orange, mean, yes);
+-- INSERT INTO bears (age, gender, color, temperament, alive) VALUES (2, male, purple, crazy, yes);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
